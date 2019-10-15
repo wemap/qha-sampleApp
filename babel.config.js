9e6d680fbb12be3b15f5b4c86be07ddc05d376c5
bertrandmd <bertrand.md@gmail.com>
2019-10-15 16:17:46 +0200

    [babel] update babel config

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,16 @@
 module.exports = {
-  presets: ['module:metro-react-native-babel-preset'],
+    presets: ['module:metro-react-native-babel-preset'],
+    plugins: [
+        [
+            'module-resolver',
+            {
+                alias: {
+                    // define assets folder
+                    'qha-assets': './assets/images',
+                    // use only one version of react (avoid breaking hooks)
+                    react: './node_modules/react',
+                },
+            },
+        ],
+    ],
 };
