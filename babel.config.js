58b1b7578103506aff3e9807362711d378ec2ac2
bertrandmd <bertrand.md@gmail.com>
2019-10-15 17:41:04 +0200

    [babel] update config

--- babel.config.js
+++ babel.config.js
@@ -9,6 +9,8 @@ module.exports = {
                     'qha-assets': './assets/images',
                     // use only one version of react (avoid breaking hooks)
                     react: './node_modules/react',
+                    // use only one version of styled-components (avoid breaking theme)
+                    'styled-components': './node_modules/styled-components',
                 },
             },
         ],
